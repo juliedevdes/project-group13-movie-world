74aeb04610bbee2b10deeedf1b9fe8851c313625
Aleksandr Baran <lidanko12@gmail.com>
2021-09-05 10:24:53 +0300

    add spinner and notif

--- src/index.js
+++ src/index.js
@@ -7,3 +7,9 @@ import modalMovieTpl from './templates/card-movie';
 import cardTpl from './templates/card-movie-home.hbs';
 import search from './js/search';
 import startpage from './js/startpage';
+<<<<<<< Updated upstream
+=======
+import watched from './js/watched';
+import addWatched from './js/add-watched';
+import spinner from './js/spinner'
+>>>>>>> Stashed changes
--- src/js/search.js
+++ src/js/search.js
@@ -1,9 +1,12 @@
 import api from './apiService';
 import cardTpl from '../templates/card-movie-home.hbs';
-import { modalOpen, gallery, inputRef } from './refs';
+import { modalOpen, gallery, inputRef,successRef, noSuccessRef } from './refs';
 import debounce from 'lodash/debounce';
+import Spinner from './spinner';
 
 inputRef.addEventListener('input', debounce(searchMovie, 750));
+
+const spinner = new Spinner();
 
 function searchMovie(e) {
   e.preventDefault();
@@ -11,6 +14,7 @@ function searchMovie(e) {
   const inputText = e.target.value;
 
   if (inputText.length <= 1) {
+   
     return;
   }
   movieSearcher(inputText, page);
@@ -20,13 +24,26 @@ async function movieSearcher(searchText, pageNumber) {
   try {
     const data = await api.MovieSearch(searchText, pageNumber);
 
-    const result = data.results;
+    const results = data.results;
+   
 
-    if (result.length === 0) {
-      return;
+    if (results.length === 0) {
+      noResults();
+      
+     return;
     }
     clearInput();
-    createCard(result);
+    createCard(results);
+
+    if (results !== []) {
+      spinner.showSpinner();
+      successRef.textContent = `Сongratulations!!! We found ${data.total_results} results of request "${searchText}" `;
+      setTimeout(function () {
+        spinner.hideSpinner();
+    successRef.textContent = '';
+  }, 1500);
+    }
+
   } catch (error) {}
 }
 
@@ -38,4 +55,16 @@ function createCard(result) {
 
 function clearInput() {
   gallery.innerHTML = '';
+}
+
+
+function noResults() {
+  spinner.showSpinner();
+  noSuccessRef.textContent =
+    'Search result not successful. Enter the correct movie name and try again';
+  setTimeout(function () {
+    noSuccessRef.textContent = '';
+    spinner.hideSpinner();
+  }, 1500);
+  clearInput();
 }
