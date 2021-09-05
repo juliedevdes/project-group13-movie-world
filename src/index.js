f2b7d8368b7918452c0aa2261b17b9b07d0818da
Julia Ohrimenko <juliajam2497@gmail.com>
2021-09-05 13:48:41 +0300

    creates js file for team-modal

--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ import onOpenModal from './js/modal-lightbox';
 import refs from './js/refs';
 import api from './js/apiService';
 import modalMovieTpl from './templates/card-movie';
+import './js/team-modal';
