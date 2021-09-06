27fa6f2572c38be5f7f6694d2d7a181ddb8c63ee
Valerii Vitenko <valero2313@gmail.com>
2021-09-06 20:30:57 +0300

    quick fix for paths

--- src/js/modal-team-lightbox.js
+++ src/js/modal-team-lightbox.js
@@ -6,7 +6,7 @@ export default function onOpenModalTeam(event) {
     `<div class="modal">
      <button class="modal__close-btn">
       <svg class="modal__close-icon" width="30" height="30">
-        <use href="./sprite/sprite.svg#icon-close"></use>
+        <use href="./images/sprite.svg#icon-close"></use>
       </svg>
     </button>
     
