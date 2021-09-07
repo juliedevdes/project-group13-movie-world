bec32dda3a989baf441b0d348cc4fb1c8604c10a
HPavlova <77845327+HPavlova@users.noreply.github.com>
2021-09-07 20:38:05 +0300

    commit api, modal esc

--- src/js/modal-film-lightbox.js
+++ src/js/modal-film-lightbox.js
@@ -1,28 +1,44 @@
 import * as basicLightbox from 'basiclightbox';
 import modalMovieTpl from '../templates/card-movie';
 import { modalFilmOpen } from './refs';
+import api from './apiService';
 
 modalFilmOpen.addEventListener('click', onOpenModalFilm);
 
 function onOpenModalFilm(event) {
-  console.log(event);
+  console.log(event.target.dataset.id);
+  event.preventDefault();
 
-  const instance = basicLightbox.create(document.querySelector('template'), {
-    onShow: instance => {
-      instance.element().querySelector('svg').onclick = instance.close;
-    },
-  });
-  instance.show();
+  api
+    .MovieSearchId(event.target.dataset.id)
+    .then(data => {
+      if (event.target.nodeName !== 'IMG') return;
 
-  const modalContainer = document.querySelector('.modal');
-  // modalContainer.innerHTML = '';
+      const instance = basicLightbox.create(document.querySelector('template'), {
+        onShow: instance => {
+          instance.element().querySelector('svg').onclick = instance.close;
+        },
+      });
+      instance.show();
 
-  const menuMarkup = createMenuMarkup(data);
+      const modalContainer = document.querySelector('.modal');
 
-  function createMenuMarkup(data) {
-    return data.map(modalMovieTpl).join('');
-  }
-  modalContainer.insertAdjacentHTML('afterbegin', menuMarkup);
+      const menuMarkup = modalMovieTpl(data);
+
+      modalContainer.insertAdjacentHTML('afterbegin', menuMarkup);
+
+      window.addEventListener('keydown', closeModalHand);
+
+      function closeModalHand(event) {
+        if (event.code === 'Escape') {
+          instance.close();
+          window.removeEventListener('keydown', closeModalHand);
+        }
+      }
+    })
+    .catch(error => {
+      console.log('oops!');
+    });
 
   // refs.modalFilmOpen.removeEventListener('click', onOpenModalFilm);
 }
